75ae248856460cd2128a6bd58c7127da167c18dd
Victor Zucchetti <victorzucchetti2@gmail.com>
2024-07-11 12:07:20 -0300

    feat: adding feature of extract and load

--- README.md
+++ README.md
@@ -4,6 +4,22 @@ Here we' wi'll construct a Data WareHouse (DW) to analyse commodity data aiming 
 
 **Make Money For The Company!**
 
-To accomplish that, we'll do a ETL process, integrating APIs and excel sheets to extract data for read in Python, transform using dbt-core, and load using PostgreSQL.
+To accomplish that, we'll do a *ELT process*, integrating *APIs to extract data in Python*, *load using PostgreSQL*, and *transform using dbt-core*.
 
-To start the project, we'll do the extraction of data from APIs
+To start the project, we'll do the extraction of commodities data from Yahoo Finance using the library `yfinance`.
+
+After, the data was extracted we'll connect to our PostgreSQL database using the Render as server. To work the connection, it's needed a .env file with the environment variables of the database (host, port, name, user, password, schema).
+
+The aforementioned steps are in the `extract_load.py` file. Run this under the `src` folder:
+```python
+python extract_load.py
+```
+
+Libraries:
+- pandas: to create dataframes and transform data.
+- sqlalchemy: to manipulate de database.
+- python-dotevn: to control environment variables.
+- psycopg2-binary: to work with the PostgreSQL.
+- yfinance: to acess data from yahoo.
+
+dbt-core is a open-source tool that documents your code, test it and use only SQL language to realize the transformations.
--- src/extract_load.py
+++ src/extract_load.py
@@ -1,1 +1,47 @@
+import yfinance as yf
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+# Variable of commodities to extract from API
+commodities = ['CL=F', 'GC=F', 'SI=F'] #Crude Oil, Gold, Silver
+
+# Load the environment variables from .env file
+DB_HOST = os.getenv('DB_HOST_PROD')
+DB_PORT = os.getenv('DB_PORT_PROD')
+DB_NAME = os.getenv('DB_NAME_PROD')
+DB_USER = os.getenv('DB_USER_PROD')
+DB_PASSWORD = os.getenv('DB_PASSWORD_PROD')
+DB_SCHEMA = os.getenv('DB_SCHEMA_PROD')
+
+# URL to acess database
+DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+# Create the engine to connect to the database
+engine = create_engine(DATABASE_URL)
+
+#Function to extract data from Yahoo Finance
+def find_commodities_data(simbol, period='5d', interval='1d'): #parameters for the function
+    ticker = yf.Ticker(simbol) #function to access the data from Yahoo Finance
+    data = ticker.history(period=period, interval=interval)[['Close']] #return a dataframe with the close price of stock
+    data['symbol'] = simbol
+    return data
+
+# Function to append data of find_commodities_data
+def find_all_commotities_data(commodities):
+    all_data = [] #list to store the data
+    for simbol in commodities: #loop to extract data from all commodities
+        data = find_commodities_data(simbol)
+        all_data.append(data) #append the data to the list
+    return pd.concat(all_data) #return the data in a single dataframe
+
+# Saving extracted data to a postgre database
+def save_data_to_db(df, schema='public'):
+    df.to_sql('commodities', con=engine, schema=schema, if_exists='replace', index=True, index_label='Date')
+
+# Main function to run the code
+if __name__ == '__main__':
+    concatenated_data = find_all_commotities_data(commodities)
+    save_data_to_db(concatenated_data)
